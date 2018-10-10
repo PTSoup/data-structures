e1da3647ba13be8d360fd211ab3e67982d52667f
PTSoup <phaimani.souphaphone@gmail.com>
2018-10-10 01:21:15 -0500

    add push to prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,6 +3,7 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
 
   var obj = Object.create(stackMethods);
+  obj.sizeCount = 0;
 
   return obj;
 
@@ -11,12 +12,15 @@ var Stack = function() {
 var stackMethods = {
 
   // push
+  push: function() {
+    this.sizeCount++;
+  },
 
   // pop
 
   // size
   size: function() {
-    return 0;
+    return this.sizeCount;
   }
 
 };
