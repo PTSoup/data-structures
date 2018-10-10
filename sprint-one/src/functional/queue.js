096b8fa2c0643b5d95b78519fc2dbc507ba52f43
PTSoup <phaimani.souphaphone@gmail.com>
2018-10-09 23:29:04 -0500

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,14 +4,23 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
+  var queuePosition = 0;
+  var nextDelete = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[queuePosition] = value;
     size++;
+    queuePosition++;
   };
 
   someInstance.dequeue = function() {
+    storage[nextDelete] = someInstance[nextDelete];
+    delete someInstance[nextDelete];
+    size === 0 ? 0 : size--;
+    nextDelete++;
+    return storage[nextDelete - 1];
   };
 
   someInstance.size = function() {
