f513e39524d8a579bf62b2deed65d8a4f13a3e04
PTSoup <phaimani.souphaphone@gmail.com>
2018-10-10 01:05:30 -0500

    add size to functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  _.extend(obj, queueMethods);
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  // enqueue
+
+  // dequeue
+
+  // size
+
+  size: function() {
+    return 0;
+  }
+
+};
 
 
