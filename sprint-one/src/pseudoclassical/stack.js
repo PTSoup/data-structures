48ffb18920e768f2c7387d274298067cd84d02c6
PTSoup <phaimani.souphaphone@gmail.com>
2018-10-10 11:19:05 -0500

    Complete pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,24 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.obj = {};
+  this.callStack = {};
+  this.storage = {};
   this.sizeCount = 0;
 
 };
 
-// push
-Stack.prototype.push = function() {
+Stack.prototype.push = function(value) {
+  this.callStack[this.sizeCount] = value;
   this.sizeCount++;
 };
 
-// pop
-
 Stack.prototype.pop = function() {
+  this.storage[0] = this.callStack[this.sizeCount - 1];
+  delete this.callStack[this.sizeCount - 1];
   this.sizeCount === 0 ? 0 : this.sizeCount--;
+  return this.storage[0];
 };
 
-// size
 Stack.prototype.size = function() {
   return this.sizeCount;
 };
-
