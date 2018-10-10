02fe51c36f41725044962cd067dd88b605bbc5b2
PTSoup <phaimani.souphaphone@gmail.com>
2018-10-10 01:29:55 -0500

    add size to prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  //enqueue
+
+  //dequeue
+
+  //size
+
+  size: function() {
+    return 0;
+  }
+
+};
 
 
