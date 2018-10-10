8d8ba3f8977f528598bb08b507bc5e1d01a6a040
PTSoup <phaimani.souphaphone@gmail.com>
2018-10-10 01:19:48 -0500

    add size to prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var obj = Object.create(stackMethods);
+
+  return obj;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  // push
+
+  // pop
+
+  // size
+  size: function() {
+    return 0;
+  }
+
+};
 
 
